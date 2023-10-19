ef576ed969c2a80e43bcf76b4935a361a4eaea4a
americanthinker <americanthinker@gmail.com>
2023-10-19 22:44:35 +0000

    keeping tiktokwn in synch

--- tiktoken_functions.py
+++ tiktoken_functions.py
@@ -2,11 +2,16 @@ import tiktoken
 from typing import List
 
 class Tokenizer:
-    def __init__(self, price: float, model_type: str="cl100k_base", bundle: int=1000):
+    def __init__(self, price: float, model_type: str=None, encoding: str="cl100k_base", bundle: int=1000):
         self.model_type = model_type
+        if not self.model_type:
+            self.encoding = encoding
+            self.tokenizer = tiktoken.get_encoding(self.encoding)
+        else:
+            self.tokenizer = tiktoken.encoding_for_model(self.model_type)
         self.price = price
         self.bundle = bundle
-        self.tokenizer = tiktoken.get_encoding(model_type)
+        
         
     def get_cost(self, texts: List[str], return_tokens: bool=False):
         tokens = self.tokenizer.encode_batch(texts)
