575cc09dbedd7f28b7b9d7fbc95565ea7c96996a
americanthinker <americanthinker@gmail.com>
2023-11-29 18:31:05 +0000

    provided paths

--- unitesting_utils.py
+++ unitesting_utils.py
@@ -17,8 +17,15 @@ def load_impact_theory_data():
         except Exception:
             print(f'Data not available at {path}, downloading from source')
             try:
-                with urllib.request.urlopen("https://raw.githubusercontent.com/americanthinker/vectorsearch-applications/main/data/impact_theory_data.json") as url:
+                with urllib.request.urlopen("https://ra.githubusercontent.com/americanthinker/vectorsearch-applications/main/data/impact_theory_data.json") as url:
                     data = json.load(url)
                 return data
             except Exception:
-                print('Data cannot be loaded from source, please move data file to one of the above paths to run this test')
+                print('Data cannot be loaded from source, please move data file to one of these paths to run this test:\n\
+    1. "./content/impact_theory_data.json"   --> if you are in Google Colab\n\
+    2. "./data/impact_theory_data.json"      --> if you are in a local environment\n')
+
+if __name__ == '__main__':
+    data = load_impact_theory_data()
+    if data:
+        print(data[0])
