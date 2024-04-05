e79f4dd5be919654eea9913ce01080f0be51318d
americanthinker <americanthinker@gmail.com>
2024-04-05 03:03:40 +0000

    finished data ingest

--- notebooks/data_ingest/data_ingest.py
+++ notebooks/data_ingest/data_ingest.py
@@ -2,13 +2,27 @@ from pytube import YouTube, Playlist
 from camel_converter import to_snake
 import transformers
 from tqdm import tqdm
+import json
 import os
+from pathlib import Path
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
 class DataIngest:
 
-    def __init__(self, playlist_url: str) -> None:
+    def __init__(self, playlist_url: str, show_url: str=None) -> None:
         self.playlist_url = playlist_url
+        self.primary_keys =  ['video_id',
+                              'title',
+                              'length_seconds',
+                              'thumbnail_url',
+                              'keywords',
+                              'short_description',
+                              'view_count',
+                              'episode_num',
+                              'episode_url',
+                              'content'
+                             ]
+        self.show_url = show_url if show_url else Playlist(self.playlist_url).owner_url
 
     def get_playlist(self) -> Playlist:
         """Get playlist from youtube"""
@@ -40,12 +54,11 @@ class DataIngest:
 
     def transcribe_audio(self,
                          pipeline: transformers.pipeline,
-                         video_meta: dict,
+                         video_id: str,
                          return_timestamps: bool=False,
                          return_text: bool=False
                          ) -> None | str:
         """Transcribe downloaded video"""
-        video_id = list(video_meta.keys())[0]
         outputs = pipeline(f"videos/{video_id}.mp4",
                             chunk_length_s=30,
                             batch_size=24,
@@ -54,13 +67,36 @@ class DataIngest:
         transcript = outputs['text'].strip().encode(encoding='utf-8').decode()
         with open(f"transcripts/{video_id}.txt", 'w') as f:
             f.write(transcript)
-        video_meta[video_id]['text'] = transcript
         if return_text:
             return transcript
     
     def _convert_keys(self, video_meta: dict) -> dict:
         """Convert keys to snake_case"""
         return {to_snake(k):v for k,v in video_meta.items()}
+    
+    def get_audio_files(self,
+                        video_urls: list[str], 
+                        video_dir: str='videos/',
+                        return_dict: bool=True,
+                        video_id_key: str='video_id'
+                        ) -> dict | list[dict]:
+        '''
+        Sequential method for multiple video downloads. Downloads 
+        YouTube videos as audio files and returns a dictionary with
+        or a list of singular metadata dictionaries.
+        '''
+        meta_data = {} if return_dict else []
+        for i, url in enumerate(tqdm(video_urls)):
+            try:
+                video_info = self.download_audio(url, i, video_dir)
+                if return_dict:
+                    meta_data[video_info[video_id_key]] = video_info
+                else:
+                    meta_data.append(video_info)
+            except Exception as e:
+                print(e)
+                continue
+        return meta_data
     
     def get_audio_files_threaded(self,
                                  video_urls: Playlist | list[str], 
@@ -89,3 +125,89 @@ class DataIngest:
                     continue
                 progress.update(1)
         return meta_data
+    
+    def read_json(self, path: str) -> dict:
+        """Read json file"""
+        with open(path, 'r') as f:
+            return json.loads(f.read()) 
+    
+    def write_json(self, data: dict, path: str) -> None:
+        """Write json file"""
+        with open(path, 'w') as f:
+            f.write(json.dumps(data, indent=4)) 
+    
+    def get_transcript_paths(self, path: str) -> list[Path]:
+        """Get transcript paths"""
+        return [path for path in Path(path).iterdir() if path.name.endswith('.txt')]
+    
+    def _get_thumbnail_url(self, episode_dict: str, default_url: str=''):
+        """
+        Get thumbnail url from episode dict.  If not found, returns self.show_url
+        as a backup url.
+        """
+        thumbnail_dict = episode_dict.get('thumbnail')
+        if thumbnail_dict:
+            thumbnail_list = thumbnail_dict.get('thumbnails')
+            if any(thumbnail_list):
+                try:
+                    return thumbnail_list[1]['url']
+                except IndexError:
+                    return thumbnail_list[0]['url']
+            else: 
+                print('thumbnail list is empty')
+                return self.show_url
+        else: 
+            print('thumbnail key not found')
+            return self.show_url
+
+    def _create_temp_dict(self, meta_data: list[dict]) -> dict:
+        """
+        Create a temporary dictionary for joining metadata
+        with transcripts
+        """
+        temp_dict = {}
+        for d in meta_data:
+            key = d['video_id']
+            temp_dict[key] = d
+        return temp_dict
+    
+    def _remove_keys(self, episode_dict: dict, keys_to_keep: list[str]=None) -> dict:
+        """
+        Remove uneeded keys from dictionary
+        """
+        keys_to_keep = keys_to_keep if keys_to_keep else self.primary_keys
+        return {k:v for k,v in episode_dict.items() if k in keys_to_keep}
+    
+    def join_single_transcript_to_meta( self,
+                                        transcript_path: Path, 
+                                        temp_dict: dict, 
+                                        content_key: str='content',
+                                        thumbnail_key: str='thumbnail_url'
+                                      ) -> None:
+        """
+        Executes in-memory insertion of content_key (transcript) and 
+        thumbnail_url key into temp_dict (metadata dictionary).
+        """
+        video_id = transcript_path.name.split('.')[0]
+        if video_id in temp_dict:
+            with open(transcript_path) as f:
+                text = f.read()
+                temp_dict[video_id][content_key] = text
+                temp_dict[video_id][thumbnail_key] = self._get_thumbnail_url(temp_dict[video_id])
+
+    def join_all_transcripts_to_meta(self,
+                                     transcript_paths: list[Path],
+                                     meta_data: list[dict],
+                                     content_key: str='content',
+                                     thumbnail_key: str='thumbnail_url',
+                                     keys_to_keep: list[str]=None
+                                    ) -> dict:
+        """
+        Join all transcripts to metadata dictionary
+        """
+        temp_dict = self._create_temp_dict(meta_data)
+        for path in transcript_paths:
+            self.join_single_transcript_to_meta(path, temp_dict, content_key, thumbnail_key)
+        temp_list = list(temp_dict.values())
+        final_list = [self._remove_keys(d, keys_to_keep) for d in temp_list]
+        return final_list
